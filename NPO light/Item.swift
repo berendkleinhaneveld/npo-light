6a7bc730a757e3e7025ff2cb1956f401639a9baa
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2026-08-29 19:54:29 +0200

    Initial Commit

---
+++ NPO light/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  NPO light
+//
+//  Created by Berend Klein Haneveld on 29/08/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
